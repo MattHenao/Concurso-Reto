66fdb94d4d8f49cca3f656198d61bd98e3b3b392
Daniel castaño <drcastanoj@gmail.com>
2021-12-24 11:38:40 -0500

    Reto

--- src/model/instructions.js
+++ src/model/instructions.js
@@ -1,14 +1,14 @@
 //const { pausa } = require('./menu')
-const  read =  require('readline');
+const read = require("readline");
 
 const instruction = () => {
-    return new Promise(resolve => {
-        console.clear();
-        console.log('======================================');
-        console.log('       prueba de instrucciones');
-        console.log('======================================\n');
+  return new Promise((resolve) => {
+    console.clear();
+    console.log("======================================");
+    console.log("       prueba de instrucciones");
+    console.log("======================================\n");
 
-        console.log(`El juego consiste en responder una serie
+    console.log(`El juego consiste en responder una serie
 de preguntas con 4 posibles respuestas.
 Consta de 5 rondas, y cada ronda consta
 de 5 preguntas. En cada ronda se va aumentado
@@ -21,19 +21,18 @@ cuidado. Y por ultimo, para guardar tu progreso, al
 final de cada ronda puedes guardarlo o seguir,
 puedes presionar "5" para salir de la partida y
 guardar progreso.
-SUERTE!!!! :DDD`)
-const readline = read.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-readline.question(`Presione ENTER para regresar al menu`, (opt) => {
-    readline.close();
-    resolve(opt);
-})
-    })
-}
-
+SUERTE!!!! :DDD`);
+    const readline = read.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    readline.question(`Presione ENTER para regresar al menu`, (opt) => {
+      readline.close();
+      resolve(opt);
+    });
+  });
+};
 
 module.exports = {
-    instruction
-}
+  instruction,
+};
